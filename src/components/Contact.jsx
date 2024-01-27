8b604ec4bdea69241570a524da6cd229bb1cf7e1
Josh484 <josh.m.mendoza1@gmail.com>
2024-01-27 15:43:20 +0000

    created a working form using handler

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+function Contact() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        description: ''
+    });
 
-function Contact(){
+    const handleInputChange = (event) => {
+        // Getting the value and name of the input which triggered the change
+        const { name, value } = event.target;
     
+        // Updating the input's state
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+      };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        
+        alert(`Hello ${formData.name}, You're email has been sent`);
+        setFormData({
+        name: '',
+        email: '',
+        description: ''
+      });
+    }
+
+
+    return (
+        <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="input" placeholder="name@example.com"onChange={handleInputChange} value={formData.name} name='name' required/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com"onChange={handleInputChange} value={formData.email} name='email'/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control as="textarea" rows={3} type='input' value={formData.description} onChange={handleInputChange} name='description'/>
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={handleFormSubmit}>
+                Submit
+            </Button>
+        </Form>
+    );
 }
 
 export default Contact
