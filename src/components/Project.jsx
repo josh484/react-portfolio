a0cb4c5be2198adb8f12dda04506ff4c2a980621
Josh484 <josh.m.mendoza1@gmail.com>
2024-01-27 11:31:18 +0000

    tried using a bootstrap carousel but it requires js, installed bootstrap-react to test new carousel

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 
 
 function Project(prop) {
-    <>
-        <div className="card bg-dark text-white">
-            <img className="card-img" src="..." alt="Card image" />
-            <div className="card-img-overlay">
-                <h5 className="card-title">{prop.name}</h5>
-                <p className="card-text">{prop.description}</p>
-                <p className="card-text">Last updated 3 mins ago</p>
+    if (prop.id == 1){
+        return (
+            <>
+            <div className="carousel-item active">
+                <img src={prop.image} alt="..."/>
+                    <div className="carousel-caption d-none d-md-block">
+                        <h5>{prop.name}</h5>
+                        <p>...</p>
+                    </div>
             </div>
-        </div>
-    </>
+        </>
+        )
+    }
+    else{
+    return (
+        <>
+            <div className="carousel-item">
+                <img src={prop.image} alt="..."/>
+                    <div className="carousel-caption d-none d-md-block">
+                        <h5>{prop.name}</h5>
+                        <p>...</p>
+                    </div>
+            </div>
+        </>
+    )
+    }
 }
 
 export default Project
--- src/components/ProjectGallery.jsx
+++ src/components/ProjectGallery.jsx
@@ -1,23 +1,43 @@
 import React, { useState } from 'react';
 import Data from './projects.json'
 import Project from './Project';
+import Carousel from 'react-bootstrap/Carousel';
 
 function ProjectGallery() {
     const [projects, setProjects] = useState(Data);
     return (
-        <div>
-            <p>hello world</p>
-            {projects.map((projects) => (
-                <Project
-                    id={projects.id}
-                    key={projects.id}
-                    name={projects.name}
-                    image={projects.image}
-                    description={projects.description}
-                />
-            ))}
+        <div id ='carousel'>
+            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div className="carousel-inner">
+                    {projects.map((projects) => (
+                        <Project
+                            id={projects.id}
+                            key={projects.id}
+                            name={projects.name}
+                            image={projects.image}
+                            description={projects.description}
+                        />
+                    ))}
+                </div>
+                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
         </div>
+
+        
     )
 }
+
 
 export default ProjectGallery
