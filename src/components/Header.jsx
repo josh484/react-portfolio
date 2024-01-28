2daed0add1f6f9bd227c3d46814a2eb9287bf211
Josh484 <josh.m.mendoza1@gmail.com>
2024-01-28 10:49:47 +0000

    added a div to about me and made some box shadows to make it stand out more

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../styles/header.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,26 +2,34 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import me from '../assets/images/me.jpeg'
 
 function Home() {
     return (
-            <Container id='frontGrid'>
-                <Row id='frontRow'>
-                    <Col xs={12} lg={6} id='aboutMe'>
-                        <h1>About Me</h1>
+        <Container id='frontGrid'>
+            <Row id='frontRow'>
+                <Col xs={12} lg={6} id='leftMe'>
+                    <div id='aboutMe'>
+                        <h1>Hi,</h1>
+                        <h1>I'm Joshua Mendoza</h1>
+                        <h2>A Front-End Web Developer</h2>
+                        <Button variant="primary" id='buttonC'>
+                            My CV
+                        </Button>
                         <div>
-                            as
+                            zz
                         </div>
-                    </Col>
-                    <Col xs={12} lg={6} id='rightMe'>
-                        <div className="jumboPicture text-center">
-                            <img src={me} className="rounded-circle" alt="Picture Of Me"/>
-                        </div>
-                    </Col>
+                    </div>
+                </Col>
+                <Col xs={12} lg={6} id='rightMe'>
+                    <div className="jumboPicture text-center">
+                        <img src={me} className="rounded-circle" alt="Picture Of Me" />
+                    </div>
+                </Col>
 
-                </Row>
-            </Container>
+            </Row>
+        </Container>
     );
 }
 
